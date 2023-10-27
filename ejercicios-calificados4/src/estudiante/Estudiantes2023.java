b5cdf71caff0457e46822559b35b2955eef94914
CaloggeroMaurizio <142445457+CaloggeroMaurizio@users.noreply.github.com>
2023-10-27 15:46:03 -0500

    Maurizio Caloggero

---
+++ ejercicios-calificados4/src/estudiante/Estudiantes2023.java
@@ -0,0 +1,82 @@
+package estudiante;
+
+import java.util.*;
+import javax.swing.*;
+import java.io.*;
+
+public class Estudiantes2023 {
+    private static ArrayList<Estudiante> estudiantes;
+
+    public Estudiantes2023() {
+        estudiantes = new ArrayList<>();
+        cargarArchivo();
+    }
+
+    public void adicionar(Estudiante estudiante) {
+        //Introduce c�digo
+    }
+
+    public void eliminar(Estudiante estudiante) {
+        //Introduce c�digo
+    }
+
+    public Estudiante obtener(int pos) {
+	//Introduce c�digo
+        return null;
+    }
+
+    public Estudiante buscar(int codigo) {
+	//Introduce c�digo        
+	  return null;
+    }
+
+    public int tamano() {
+        //Introduce c�digo        
+	  return estudiantes.size();
+	}
+
+    public void listarEstudiantes(){
+        for (Estudiante estudiante : estudiantes) {
+        	System.out.println("Codigo: " + estudiante.getCodigo() +
+        			", Nombre: " + estudiante.getNombre() + "Ciclo: " + estudiante.getCiclo() +
+        			"Pension: " + estudiante.getPension());
+        }
+    }
+
+    // M�todos para manipular el archivo de texto
+    private void readFromInputStream(InputStream inputStream) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+        String linea;
+        while ((linea = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(linea, ",");
+            createStudent(st);
+        }
+        br.close();
+    }
+
+	// M�todo que a�adir a los estudiantes del archivo .txt en el arrayList estudiantes.
+    private void createStudent(StringTokenizer st){
+        int codigo = Integer.parseInt(st.nextToken().trim());
+        String nombre = st.nextToken().trim();
+        int ciclo = Integer.parseInt(st.nextToken().trim());
+        double pension = Double.parseDouble(st.nextToken().trim());
+        Estudiante estudiante = new Estudiante(codigo, nombre, ciclo, pension);
+        adicionar(estudiante);
+    }
+
+    // M�todos para manipular el archivo de texto
+    private void cargarArchivo() {
+        try {
+            File file = new File("./src/estudiantes.txt");
+            Class<Estudiantes2023> clazz = Estudiantes2023.class;
+            InputStream inputStream = clazz.getResourceAsStream("/estudiantes.txt");
+            if (file.exists()) {
+                readFromInputStream(inputStream);
+            } else
+                JOptionPane.showMessageDialog(null,
+                        "El archivo estudiantes.txt no existe");
+        } catch (Exception x) {
+            JOptionPane.showMessageDialog(null, "Se produjo un error= " + x);
+        }
+    }
+}
---
+++ ejercicios-calificados4/src/estudiante/Main.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+package estudiante;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Estudiantes2023 estudiantes = new Estudiantes2023();
+        estudiantes.listarEstudiantes();
+    }
+}
