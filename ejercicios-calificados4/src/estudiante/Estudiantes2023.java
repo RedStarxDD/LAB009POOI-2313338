8962a41b639592b2dfa6874c0a389f7fdee7d4b7
Dante <143035058+RedStarxDD@users.noreply.github.com>
2023-10-27 19:42:40 -0500

    Dante Uchofen Grupo 5

--- ejercicios-calificados4/src/estudiante/Estudiantes2023.java
+++ ejercicios-calificados4/src/estudiante/Estudiantes2023.java
@@ -1,7 +1,6 @@
 package estudiante;
 
 import java.util.*;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import javax.swing.*;
@@ -14,30 +13,33 @@ public class Estudiantes2023 {
         estudiantes = new ArrayList<>();
         cargarArchivo();
     }
+    
     public void adicionar(Estudiante estudiante) {
 		// TODO Auto-generated method stub
     	estudiantes.add(estudiante);
 	}
+    
     public void añadir() {
         Scanner scanner = new Scanner(System.in);
-        	System.out.println("Ingrese nombre del estudiante: ");
-        	String nombre = scanner.nextLine();
-        	
-        	System.out.println("Ingrese apellido del estudiante: ");
-        	String apellido = scanner.nextLine();
-        	
-        	System.out.println("Ingrese ciclo del estudiante: ");
-        	int ciclo = scanner.nextInt();
-        	
-        	System.out.println("Ingrese pension del estudiante: ");
-        	double pension = scanner.nextDouble();
-        
-        	Estudiante es = new Estudiante(0,nombre+" "+apellido, ciclo, pension);
-        	adicionar(es);
-        	
-        	System.out.println("Se añadio con exito!");
-    
-    		listarEstudiantes();
+    	System.out.println("Ingrese nombre del estudiante: ");
+    	String nombre = scanner.nextLine();
+    	
+    	System.out.println("Ingrese apellido del estudiante: ");
+    	String apellido = scanner.nextLine();
+    	
+    	System.out.println("Ingrese ciclo del estudiante: ");
+    	int ciclo = scanner.nextInt();
+    	
+    	System.out.println("Ingrese pension del estudiante: ");
+    	double pension = scanner.nextDouble();
+    	
+    	int codigo=estudiantes.get(estudiantes.size() - 1).getCodigo()+1;
+    	Estudiante es = new Estudiante(codigo,nombre+" "+apellido, ciclo, pension);
+    	adicionar(es);
+    	
+    	System.out.println("Se añadio con exito!");
+
+		listarEstudiantes();
     }
 
       public void eliminar() {
@@ -55,9 +57,37 @@ public class Estudiantes2023 {
     	listarEstudiantes();
 	}
 
-    public Estudiante obtener(int pos) {    
+    public Estudiante obtener(int pos) {
+	//Introduce c�digo
         return null;
     }
+
+    public void buscarPorCodigo() {
+    	Scanner scanner = new Scanner(System.in);
+    	
+		System.out.println("Ingrese el codigo del estudiante: ");
+		  int codigoBuscar = scanner.nextInt();
+		  
+		  for (Estudiante estudiante : estudiantes) {
+			if(estudiante.getCodigo()==codigoBuscar) {
+				System.out.println("Se ha encontrado: Código " + estudiante);
+				return;
+			}
+		  }
+    }
+    
+    public void buscarPorApellido() {
+    	Scanner scanner = new Scanner(System.in);
+		 System.out.println("Ingrese el nombre y apellido del estudiante: ");
+		 String apellidoBuscar = scanner.nextLine();
+		 for (Estudiante estudiante : estudiantes) {
+			if(estudiante.getNombre().equalsIgnoreCase(apellidoBuscar)) {
+				System.out.println("Se ha encontrado: Apellido"+ estudiante);
+				return;
+			}
+		 }
+    }
+    
     public void sumarPensiones() {
     	double sumatotal=0;
     	
@@ -66,30 +96,7 @@ public class Estudiantes2023 {
 		}
     	System.out.println("La suma total de las pensiones es:"+sumatotal);
     }
-
-    public Estudiante buscar(int codigo) {
-	  Scanner scanner = new Scanner(System.in);
-
-    	System.out.println("Ingrese el codigo del estudiante: ");
-        	  int codigoBuscar = scanner.nextInt();
-        	  
-        	  for (Estudiante estudiante : estudiantes) {
-				if(estudiante.getCodigo()==codigoBuscar) {
-					System.out.println("Se ha encontrado: Codigo " + estudiante.getCodigo());
-					break;
-				}
-        	  }
-        	  Scanner scanner1 = new Scanner(System.in);
-
-          	System.out.println("Ingrese el apellido del estudiante a elimminar: ");
-              	 String codigoBuscar1 = scanner1.nextLine();
-              	  
-              	  for (Estudiante estudiante : estudiantes) {
-      				if(estudiante.getNombre()==codigoBuscar1) {
-      					System.out.println("Se ha encontrado: Apellido"+ estudiante.getNombre());
-      					break;
-}
-
+    
     public int tamano() {
         //Introduce c�digo        
 	  return estudiantes.size();
@@ -97,31 +104,42 @@ public class Estudiantes2023 {
 
     public void listarEstudiantes(){
         for (Estudiante estudiante : estudiantes) {
-        	System.out.println("Codigo: " + estudiante.getCodigo() +
+        	System.out.println("Código: " + estudiante.getCodigo() +
         			", Nombre: " + estudiante.getNombre() + ", Ciclo: " + estudiante.getCiclo() +
-        			", Pension: " + estudiante.getPension());
+        			", Pensión: " + estudiante.getPension());
         }
     }
     
-    public void ordenarEstudiantesPorApellido() { 
- 	   List<Estudiante> estudiantesOrdenados=estudiantes.stream().sorted(Comparator.comparing(Estudiante::getNombre)).collect(Collectors.toList());
- 	   
- 	   for (Estudiante estudiante : estudiantesOrdenados) {
- 		System.out.println(estudiante);
- 	}
+    public void ordenarEstudiantes(String tipo) {
+    	List<Estudiante> estudiantesOrdenados=new ArrayList<Estudiante>();
+    	
+    	switch (tipo) {
+		case "apellido":
+		 	   estudiantesOrdenados=estudiantes.stream().sorted(Comparator.comparing(Estudiante::getNombre)).collect(Collectors.toList());			
+			break;
+		case "pension":
+			estudiantesOrdenados=estudiantes.stream().sorted(Comparator.comparingDouble(Estudiante::getPension)).collect(Collectors.toList());			break;
 
+		default:
+			break;
+		}
+ 	   
+    	for (Estudiante estudiante : estudiantesOrdenados) {
+    		System.out.println(estudiante);
+ 	   	}
      }
     
-    public void ordenarEstudiantesPorPension() { 
- 	   List<Estudiante> estudiantesOrdenados=estudiantes.stream().sorted(Comparator.comparingDouble(Estudiante::getPension)).collect(Collectors.toList());
- 	   
- 	   for (Estudiante estudiante : estudiantesOrdenados) {
- 		System.out.println(estudiante);
- 	}
-
-     }
-    
-
+    public void modificarMinusculas() {
+    	for (Estudiante estudiante : estudiantes) {
+			if(estudiante.getNombre().substring(0,1).equals(estudiante.getNombre().substring(0,1).toLowerCase())){
+				estudiante.setNombre(estudiante.getNombre().substring(0,1).toUpperCase()+estudiante.getNombre().substring(1));
+			}
+		}
+    	
+    	System.out.println("Valores cambiados");
+    	listarEstudiantes();
+    }
+        
     // M�todos para manipular el archivo de texto
     private void readFromInputStream(InputStream inputStream) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
